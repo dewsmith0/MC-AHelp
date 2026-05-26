f015513c588cd2cb9f3410ce4d45763b790a47b7
Dewsmith <23081124+dewsmith0@users.noreply.github.com>
2026-05-26 16:40:24 -0300

    Fixed color in ahelp broadcast

--- src/main/java/io/github/dewsmith0/mcahelp/AhelpSender.java
+++ src/main/java/io/github/dewsmith0/mcahelp/AhelpSender.java
@@ -9,13 +9,12 @@ import org.bukkit.entity.Player;
 public class AhelpSender {
     private static final MCAhelp plugin = MCAhelp.getPlugin(MCAhelp.class);
     public static void sendAhelpToAdmins(Player sender, String message) {
-        Component formattedMessage = MiniMessage.miniMessage().deserialize("<light_aqua>[AHelp] <sender>:</light_aqua> <message>",
+        Component formattedMessage = MiniMessage.miniMessage().deserialize("<aqua>[AHelp] <sender>:</aqua> <message>",
                 Placeholder.unparsed("sender", sender.getName()), Placeholder.unparsed("message", message));
         AhelpHistory.addLog(sender, sender, message);
         for (OfflinePlayer operator : plugin.getServer().getOperators()) {
             if(operator.isOnline() && operator.getPlayer() != null) {
                 operator.getPlayer().sendMessage(formattedMessage);
-
             }
 
 
